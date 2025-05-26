070b6e3aef12ccbd0ce87900748c562f73154fc4
BranK <23649425+ItsBranK@users.noreply.github.com>
2025-05-26 17:09:14 -0400

    Update Changelog.hpp
    - Fixed a lot of spelling mistakes.

--- Changelog.hpp
+++ Changelog.hpp
@@ -1,5 +1,8 @@
 #pragma once
 /*
+    Changes in v1.6.0:
+    
+
     Changes in v1.5.9:
     - Added a new "EventTypes" enum in "Types.hpp", which is now used by the hook classes in "Events.hpp/cpp" to let you tell where a function was called from.
     - Changed the "ProcessBefore" and "ProcessAfter" functions to use the new "EventTypes" enum.
@@ -7,7 +10,7 @@
     Changes in v1.5.8:
     - BREAKING: Renamed the "QueueData" class in "Manager.hpp/cpp" to "ManagerQueue".
     - BREAKING: Changed the "ToHex" notion parameters for the functions in "Formatting.hpp/cpp".
-    - Added two new virutal functions for the "Module" class in "Module.hpp/cpp", "OnCreate" and "OnDestroy", which work the same way as the ones from the "Component" class.
+    - Added two new virtual functions for the "Module" class in "Module.hpp/cpp", "OnCreate" and "OnDestroy", which work the same way as the ones from the "Component" class.
     - Added two new random string generator functions "RandomString" and "RandomCharacter" in "Formatting.hpp/cpp".
     - Added a new templated "Shuffle" function in "Formatting.hpp/cpp".
     - Improvements to the "VariableComponent::CreateSetting" and "VariableComponent::CreateCommand" functions in "Variables.hpp/cpp".
@@ -29,7 +32,7 @@
 
     Changes in v1.5.6:
     - BREAKING: Moved the "DisableThreadLibraryCalls" function call from "Core.cpp" to "dllmain.cpp".
-    - BREAKING: Renamed the function "CoreComponent::InitializeGlobals" to "CoreComponent::OnThreadCreated" in "Core.hpp/cpp", and added comments for better explainnation for why this is needed.
+    - BREAKING: Renamed the function "CoreComponent::InitializeGlobals" to "CoreComponent::OnThreadCreated" in "Core.hpp/cpp", and added comments for better a explanation for why this is needed.
     - Made the "m_mainThread" property from "CoreComponent" to be both static and atomic, in "Core.hpp".
 
     Changes in v1.5.5:
@@ -44,7 +47,7 @@
     - Various code organizations and refactoring throughout the project.
 
     Changes in v1.5.4:
-    - Added the "EObjectFlags::RF_RootSet" flag and removed "EObjectFlags::RF_Transient" for the "MarkInvincible/MarkForDestroy" fucntions in "Instances.hpp/cpp".
+    - Added the "EObjectFlags::RF_RootSet" flag and removed "EObjectFlags::RF_Transient" for the "MarkInvincible/MarkForDestroy" functions in "Instances.hpp/cpp".
 
     Changes in v1.5.3:
     - Internal improvements and optimizations to setting functions in "Manager.cpp".
@@ -62,7 +65,7 @@
     Changes in v1.5.0:
     - Added a new "ManagerComponent::OnCanvasDraw" function to control all the individual canvas drawing functions for each module, this should be called by your hud render function.
     - Added a new virtual void "Module::OnCreateVariables" for modules where you can create module specific settings or commands instead of putting them all in the "ManagerComponent::Initialize()" function. This is called by the "ManagerComponent::CreateModule" function in "Manager.cpp".
-    - Changed the "UpdateSettings" function from the placeholdder mod to be a virtual void and renamed it to "OnSettingChanged", this should be called by your function when its value has changed.
+    - Changed the "UpdateSettings" function from the placeholder mod to be a virtual void and renamed it to "OnSettingChanged", this should be called by your function when its value has changed.
     - Added a new virtual void "Module::OnCanvasDraw" for modules, this is called by the new "ManagerComponent::OnCanvasDraw" function in "Manager.cpp".
     - The "ManagerComponent::OnTick" function is now called by the new "ManagerComponent::OnCanvasDraw" function in "Manager.cpp".
 
@@ -70,7 +73,7 @@
     - Added two new settings for commands called "IsLocked" and "NeedsArgs", locked prevents the command from being called, and needs args states that the command requires some sort of argument to be passed through. Setting this false means arguments are optional for the command.
     - Added a new "Variable" class which both the "Setting" and "Command" class now inherit from, this stores the variable and internal flags / permissions for said variable.
     - Added two new internal constants, "VARIABLE_NAME_LENGTH" and "VARIABLE_VALUE_LENGTH", in Manager.cpp" which let you set the max length for variable values and names.
-    - BREAKING: Made the "ManagerComponent::CreateVariable" functon private, as a result the "CreateCommand" and "CreateSetting" now require an additional argument for creating them for their names.
+    - BREAKING: Made the "ManagerComponent::CreateVariable" function private, as a result the "CreateCommand" and "CreateSetting" now require an additional argument for creating them for their names.
     - BREAKING: Got rid of all the optional constructors for the "Setting" class in "Manager.hpp/cpp", you should now call the setter functions to bind specific callbacks or set permissions.
     - BREAKING: Renamed the "IsSearchable" function for the command and setting classes to to "IsHidden" in "Manager.hpp/cpp".
     - BREAKING: Renamed the "Command::BindCallback" function to "Command::BindStringCallback" in "Manager.hpp/cpp".
@@ -154,7 +157,7 @@
     - Added three new functions, "Lerp", "Midpoint" and "DistanceTo", to all the vector classes in "Math.hpp/cpp".
     - Added a built in "VectorF::Rotate" function  in "Math.hpp/cpp".
     - Added a new templated "RandomRange" function in the "Math" namespace in "Math.hpp".
-    - Added the "<numeric>" and "<random>" headers include to "pch.hpp" because thats needed for the new/updated functions in "Math.cpp".
+    - Added the "<numeric>" and "<random>" headers include to "pch.hpp" because that's needed for the new/updated functions in "Math.cpp".
     - Added new "Formatting::ReplaceAllChars" and "Formatting::Contains" functions in "Formatting.hpp/cpp".
     - Replaced all the "IsChar" functions in "Formatting.hpp/cpp" and replaced them with C++ library functions instead.
     - Changed the "ToLower" and "ToUpper" functions to use "std::transform" instead in "Formatting.cpp".
@@ -164,7 +167,7 @@
     Changes in v1.3.3:
     - Revamped the console command and queue system, replaced it with "InternalCommand" and "ConsoleCommand" functions, they now automatically queue based on which thread you are calling them from!
     - Added a bunch of extra functions to the "Color" and "LinearColor" classes, they can convert the underlying color values to decimal or hex in "Colors.hpp/cpp".
-    - Added some template saftey checks in "Instances.hpp".
+    - Added some template safety checks in "Instances.hpp".
     - Added a "Conjugate" function to the "Quat" class in "Math.hpp/cpp"
     - Changed how the "UnrealCommand" function works in "Manager.cpp", added checks to remove the original command and only pass the arguments.
     - Changed how color values are stored in the "Setting" class in "Manager.cpp", they are now stored as their hex values instead of decimal.
@@ -175,13 +178,13 @@
     Changes in v1.3.2:
     - Added some console logging ifdefs to remind new people using this template how it works.
     - Added some extra null checks for the functions in "Instances.hpp".
-    - Added a default bool argument for the "SetStringValue" function in the "Setting" class called "bOverride", if set to true when setting a string it skips and saftey checks internally.
+    - Added a default bool argument for the "SetStringValue" function in the "Setting" class called "bOverride", if set to true when setting a string it skips and safety checks internally.
     - Changed the "ResetToDefault" function in "Setting" class to use the new bool argument set to true as explained above.
     - Fixed color values not resetting to default properly if you called the function from a different thread.
     - Fixed the "DecimalToHex" function in "Colors.cpp".
 
     Changes in v1.3.1:
-    - Remade and added tons of extra functions to the "Setting" class in "Manager.hpp/cpp", the additions and changes include all new range and value functions, as well as extra saftey checks.
+    - Remade and added tons of extra functions to the "Setting" class in "Manager.hpp/cpp", the additions and changes include all new range and value functions, as well as extra safety checks.
     - Remade and added a bunch of extra helper functions in "Formatting.hpp", you should uh check them out if you want.
     - Changed the "AreGlobalsValid" function in "Core.cpp".
 
@@ -190,7 +193,7 @@
     - Improved and fixed various operators and functions for the color and math classes in both "Colors.hpp/cpp" and "Math.hpp/cpp".
 
     Changes in v1.2.9:
-    - Fixed the "IsStringNumber" and "IsStringFloat" functions from returning false if the given string was negitive in "Formatting.cpp".
+    - Fixed the "IsStringNumber" and "IsStringFloat" functions from returning false if the given string was negative in "Formatting.cpp".
     - Added two new functions, "ToLinear" and "ToColor", to the color structs in "Colors.hpp" which auto converts color types from one to the other.
     - Added a new setting type, "TYPE_ROTATOR", and added a "GetRotatorValue" to accommodate this for the "Setting" class in "Manager.hpp/cpp".
     - Added a "GetQuat" function which returns a Quat version of a Rotator in "Math.hpp/cpp".
@@ -205,7 +208,7 @@
 
     Changes in v1.2.7:
     - Added four new functions in "Formatting.hpp", "IsStringNumber", "IsStringFloat", "EraseAllChars", and "RemoveAllChars".
-    - Fixed a bug in the "SetValue" function that prevented some setitng types from applying in "Manager.cpp".
+    - Fixed a bug in the "SetValue" function that prevented some setting types from applying in "Manager.cpp".
     - Made the "DecimalToHex" function have a fill of 6 now in "Colors.cpp".
     - BREAKING: Renamed the function "AddToQueue" to "QueueCommand" and changed the "CommandQueue" to use a new custom class in "Manager.hpp/cpp".
 
@@ -219,7 +222,7 @@
     - Added an option "bLogToConsole" argument for the "ConsoleCommand" function in "Manager.hpp".
     - Added logging the old vs new value when changing a settings value for the "ConsoleCommand" function in "Manager.cpp".
     - Added a range feature for the "Setting" class, you can now specific a minimum and maximum range when setting values now. The new functions are "HasRange", "InRange", and "SetRange".
-    - Added an example of how to use the new "SetRange" funtion in the "Initialize" function in "Manager.cpp".
+    - Added an example of how to use the new "SetRange" function in the "Initialize" function in "Manager.cpp".
     - Added a "Searchable" option for the "Command" class in "Manager.hpp", it has a default value of "true" in the classes constructor.
     - Added new functions, "GetId" and "GetDefaultValue", for the "Setting" class in "Manager.hpp".
     - Remade how binding and triggering setting/command callbacks work completely, and removed storing argument data in the "Command" class itself.
@@ -235,7 +238,7 @@
     - BREAKING: Added/renamed functions in the "GameState.hpp/cpp" files and made them more organized.
 
     Changes in v1.2.3:
-    - Added an argument callback option for the "Setting" class in "Manager.hpp", works similar to the "Command" classes arugment callback but the argument is the string value of the setting.
+    - Added an argument callback option for the "Setting" class in "Manager.hpp", works similar to the "Command" classes argument callback but the argument is the string value of the setting.
     - Added an optional argument for the "UnrealCommand" function in "Manager.hpp" called "bLogToConsole".
     - Fixed the "Lerp" function for the "VectorI" class taking a float instead of an integer in "Math.hpp".
     - Fixed the operators for the "Rotator" class in "Manager.cpp" saying struct instead of class.
@@ -250,7 +253,7 @@
     Changes in v1.2.1:
     - Improved memory functions in "UnrealMemory.hpp", just look at them for yourself or something idk.
     - Improved the constructors for the "Setting" and "Command" classes in "Manager.cpp".
-    - Added the virutal functions "OnCreate" and "OnDestroy" in "Component.hpp", these can be manually called outside of the classes constructor and deconstructor.
+    - Added the virtual functions "OnCreate" and "OnDestroy" in "Component.hpp", these can be manually called outside of the classes constructor and deconstructor.
     - Added a "CreatedInstances" vector in "Instances.hpp", when a custom object instance is created it is added to this vector. On the components "OnDestory" function all custom instances are marked to be destroyed as well.
     - Added the "CreateVariable" function in "Manager.hpp" and moved creating variables from :ManagerComponent"'s constuctor to the "Initialize" function in "Manager.cpp".
     - Added the "AttachDetour" and "DetachDetour" functions in "Events.hpp", this is now called from "InitializeGlobals" in "Core.cpp".
@@ -275,7 +278,7 @@
 
     Changes in v1.1.8:
     - Added the "MarkInvincible" function in "Manager.hpp" which lets you easily prevent objects from being destroyed.
-    - Added saftey checks for the "invincible" functions in "Manager.cpp".
+    - Added safety checks for the "invincible" functions in "Manager.cpp".
     - Added a null check for updating modules settings when the "Initialize" function is called in "Manager.cpp".
     - Added virtual deconstructors for components and modules.
     - Reformatted some of the constructors for the color classes in "Colors.cpp".
@@ -293,7 +296,7 @@
     - Added examples for how the "ResetSetting", "PrintModule", and "UnrealCommand" functions from "ManagerComponent" can be used as actual commands.
     - Remade all the math classes to use templated base classes in "Math.hpp", along with changing their names to pair with their underlying types.
     - Added a "Recalculate" function for the "LinearColor" class in "Colors.hpp", this is auto called in one of the constructors to recalculate the values if the user inputs an incorrect color space. For example they mistakenly put in a value from 0-255 instead of 0f-1f.
-    - Added a few const variables to operators in both "Colors.hpp" and "Math.hpp".
+    - Added a few constant variables to operators in both "Colors.hpp" and "Math.hpp".
 
     Changes in v1.1.5:
     - Changed all function hooks to use the new "PreEvent" and "PostEvent" classes, they have neat template functions that auto recast for you as well as an option to not detour the function through Process Event.
@@ -328,14 +331,14 @@
     - Added a "Quat" class in "Math.cpp", along with a bunch of defines/math helpers.
     - Added "Get" functions to the FRainbowColor class, this copies the data but guarantees you can't modify the original structs by mistake.
     - Updated the placeholder sdk with the new math structs so the project can compile.
-    - BREAKING: Renamed all "CR" math classes to just normal structs in "Math.hpp", also added extra math functions and operators for already exsiting classes.
+    - BREAKING: Renamed all "CR" math classes to just normal structs in "Math.hpp", also added extra math functions and operators for already existing classes.
     - BREAKING: Renamed all "CR" color classes to just normal structs in "Colors.hpp", also added some extra operators for them.
 
     Changes in v1.1.0:
     - Added a "WhitelistEvent" function in "Events.hpp".
     - Added the maps "StaticClasses" and "StaticFunctions" in "Instances.hpp", here all of these objects are mapped by their full name to their pointer.
-    - Added extra saftey checks and warnings for hooking functions.
-    - Changed how hooking functions, and binding them work. Now you can hook multiple voids to a singluar function, before it would be limited to only one.
+    - Added extra safety checks and warnings for hooking functions.
+    - Changed how hooking functions, and binding them work. Now you can hook multiple voids to a singular function, before it would be limited to only one.
     - Removed the toggle for logging functions in the EventsComponent, that was messy anyway.
 
     Changes in v1.0.9:
@@ -376,7 +379,7 @@
     - Added option for the bool setting to toggle true/false with 1/0.
     - Moved the null check for functions in process event.
     - Fixed the constructor for StringSetting.
-    - Removed the "Enabled" bool in the module base class, you should be adding this in your custo module class itself.
+    - Removed the "Enabled" bool in the module base class, you should be adding this in your custom module class itself.
 
     Changes in v1.0.4:
     - Added an option to blacklist events from going through Process Event.
